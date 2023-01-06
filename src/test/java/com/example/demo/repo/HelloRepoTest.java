a0659e927f2f4b1a8c7aedc0cd8cd862adfb2c75
Jasmine <jasmine.quah@thoughtworks.com>
2023-01-06 12:52:04 +0800

    [JQ] Update repo unit test to not need application context

--- src/test/java/com/example/demo/repo/HelloRepoTest.java
+++ src/test/java/com/example/demo/repo/HelloRepoTest.java
@@ -1,38 +1,50 @@
 /* (C)2023 */
 package com.example.demo.repo;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import com.example.demo.repo.po.MessagePo;
-import jakarta.transaction.Transactional;
 import java.util.List;
 import java.util.UUID;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
 
-@SpringBootTest
-@Transactional
+@ExtendWith(SpringExtension.class)
+@DataJpaTest
 class HelloRepoTest {
 
-  @Autowired HelloRepo helloRepo;
+    @Autowired
+    HelloRepo helloRepo;
 
-  @BeforeEach
-  void setUp() {
-    helloRepo.deleteAll();
-  }
+    @BeforeEach
+    void setUp() {
+        helloRepo.deleteAll();
+    }
 
-  @Test
-  void shouldReturnMessageOrdered__givenOriginalMessageNotInOrder() {
-    helloRepo.save(
-        MessagePo.builder().id(UUID.randomUUID().toString()).person("Smith").message("B").build());
-    helloRepo.save(
-        MessagePo.builder().id(UUID.randomUUID().toString()).person("Smith").message("A").build());
+    @Test
+    void shouldReturnMessageOrdered__givenOriginalMessageNotInOrder() {
+        helloRepo.save(
+            MessagePo
+                .builder()
+                .id(UUID.randomUUID().toString())
+                .person("Smith")
+                .message("B")
+                .build());
+        helloRepo.save(
+            MessagePo
+                .builder()
+                .id(UUID.randomUUID().toString())
+                .person("Smith")
+                .message("A")
+                .build());
 
-    var messages = helloRepo.findByPersonOrderByMessageAsc("Smith");
+        var messages = helloRepo.findByPersonOrderByMessageAsc("Smith");
 
-    assertEquals(2, messages.size());
-    assertEquals(List.of("A", "B"), messages);
-  }
+        assertEquals(2, messages.size());
+        assertEquals(List.of("A", "B"), messages);
+    }
 }
